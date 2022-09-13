248b0d5f1389682a8c09b31538289ca965758f30
MaksymKonoplinskyi <gran7777777@gmail.com>
2022-09-13 23:11:36 +0300

    2

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0xC03544Ed7E965A1aF337d9cfb49a9f50cB463021"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/0E3RzfF5ueQc69NQExg3FqKvK4Ila5wE",
   },
 };
